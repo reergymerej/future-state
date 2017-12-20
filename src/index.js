2ab8d798beee6e1b2ae4c5a860a6ae72959f97e7
Jeremy Greer <jeremy.greer.atl@gmail.com>
2017-12-20 15:47:52 -0600

    return a value when available (sloppy version)

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
-const app = () => Promise.resolve()
+const getFutureStoreValues = (store, value) =>
+  new Promise((resolve) => {
+    const unsubscribe = store.subscribe(() => {
+      const state = store.getState()
+      let value
+      try {
+        value = state.foo.bar.baz
+      } catch (err) {
+        console.log(err)
+      }
+      if (value !== undefined) {
+        unsubscribe()
+        resolve(value)
+      }
+    })
+  })
+
+
+const app = (store, value) => {
+  return getFutureStoreValues(store)
+}
 export default app
--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,34 @@
 import app from './index';
+import { createStore } from 'redux'
 
 describe('basic usage', () => {
   it('should return a promise', () => {
-    const store = {}
+    const store = createStore(() => {})
     const result = app(store, 'foo.bar.baz')
     expect(result.then).toBeDefined()
+  })
+
+  it('should give you the value from the store', async () => {
+    const baz = 'I am the baz!'
+    const reducer = (state = {}, action) => {
+      if (action.type === 'DO_THE_DANG_THING') {
+        return {
+          foo: {
+            bar: {
+              baz,
+            },
+          },
+        }
+      }
+      return state
+    }
+    const store = createStore(reducer)
+    setTimeout(() => {
+      store.dispatch({
+        type: 'DO_THE_DANG_THING',
+      })
+    }, 0)
+    const result = await app(store, 'foo.bar.baz')
+    expect(result).toBe(baz)
   })
 })
