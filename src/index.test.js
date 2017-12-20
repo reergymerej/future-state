78fd48d61acd9a84c88824069c2830ac94eca0eb
Jeremy Greer <jeremy.greer.atl@gmail.com>
2017-12-20 15:21:47 -0600

    use Jest

--- src/index.test.js
+++ src/index.test.js
@@ -1,10 +1,9 @@
-import { expect } from 'chai';
-import app from '../src';
+import app from './index';
 
 describe('basic usage', () => {
   it('should return a promise', () => {
     const store = {}
     const result = app(store, 'foo.bar.baz')
-    expect(result.then).to.exist
+    expect(result.then).toBeDefined()
   })
 })
